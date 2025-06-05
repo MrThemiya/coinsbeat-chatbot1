6bd130436e91e9a1a7b7787713fbaba06dd4f4bb
MrThemiya <pay.themiya@gmail.com>
2025-06-05 14:02:25 +0530

    Fix duplicate imports

--- components/CoinsbeatAIChat.jsx
+++ components/CoinsbeatAIChat.jsx
@@ -1,11 +1,7 @@
 
 
 "use client";
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
 
- 
 
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
